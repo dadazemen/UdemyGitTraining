9bde1cc4f562e9825f32c7670e85ce92b1a4811e
Dragana Zemen <dragana.zemen@gmail.com>
2023-01-25 13:55:21 +0100

    app.js in master

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ const login() {
     const elem = document.getElementById("user");
     const pwd = document.getElementById("pwd");
 }
+
+const f = (){
+    console.log("f function");
+}
+
+const validate = () {
+    // master branch
+    for (i = 0; i < 100; i++) {
+        console.log(i);
+    }
+}
